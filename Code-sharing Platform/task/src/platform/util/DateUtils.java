5df9dde5e693554c2110ab2549c3d5acb93a7abe
Ryuukae <ryuukae@gmail.com>
2024-03-22 08:27:08 -0400

    Update application paths and refactor code snippet retrieval
    Updated file paths in application properties. Refactored code retrieval mechanism in WebController, improving readability and error handling. Enhanced logging capabilities by configuring a new logback file. In addition, revised the CodeSnippet model for better code-timestamp handling. Introduced a new utility class, DateUtils, to support date formatting operations.

--- Code-sharing Platform/task/src/platform/util/DateUtils.java
+++ Code-sharing Platform/task/src/platform/util/DateUtils.java
@@ -4,8 +4,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
-	public static String formatDate(LocalDateTime date) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-		return date.format(formatter);
+	public static LocalDateTime formatDate(LocalDateTime date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		return LocalDateTime.parse(date.format(formatter), formatter);
 	}
 }
