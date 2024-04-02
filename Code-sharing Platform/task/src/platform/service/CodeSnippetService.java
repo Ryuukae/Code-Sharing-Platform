368b1a15969c6f5dfd8e3206b5fabbbc29f79296
Ryuukae <ryuukae@gmail.com>
2024-04-02 02:57:58 -0400

    Remove unused files: CodeSnippetApiResponse.java and logback.log
    Deleted the 'CodeSnippetApiResponse.java' file as it was no longer
    required in the application. This change makes the codebase cleaner and easier to maintain.

--- Code-sharing Platform/task/src/platform/service/CodeSnippetService.java
+++ Code-sharing Platform/task/src/platform/service/CodeSnippetService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import platform.model.CodeSnippet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -31,14 +32,15 @@ public class CodeSnippetService {
 		logger.info("Added new code snippet with ID: {}", snippet.getId());
 	}
 
-	// In Service method
 	public CodeSnippet[] getLatestCodeSnippets() {
 		try {
 			logger.debug("Getting latest code snippets");
-			return codeSnippets.stream()
-					       .sorted((s1, s2) -> s2.getTimestamp().compareTo(s1.getTimestamp()))
-					       .limit(10)
-					       .toArray(CodeSnippet[]::new);
+			CodeSnippet[] snippets = codeSnippets.stream()
+					                         .sorted((s1, s2) -> s2.getTimestamp().compareTo(s1.getTimestamp()))
+					                         .limit(Math.min(codeSnippets.size(), 10))
+					                         .toArray(CodeSnippet[]::new);
+			logger.debug("Latest snippets: {}", Arrays.toString(snippets));
+			return snippets;
 		} catch (NullPointerException e) {
 			logger.error("Failed to get latest code snippets due to null pointer", e);
 			return new CodeSnippet[ 0 ];
